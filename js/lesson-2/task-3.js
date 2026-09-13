d689f549c3522be4837a9226d28773d7657a2ddd
Mariia Levchenko <ml.yurievna27@gmail.com>
2026-09-13 10:33:19 +0200

    Task 2.3 update

--- js/lesson-2/task-3.js
+++ js/lesson-2/task-3.js
@@ -5,17 +5,26 @@
 
 function caclculateAverage(...arr) {
     let sum = 0;
-    for (const item of arr) {
-        if (typeof item !== 'number') {
-            return 'Not a nubmer';
-        } else if (arr.length == 0) {
-            return 'Array is empty';
+    let countNotNum = 0;
+
+    if (arr.length == 0) {
+        return 'Array is empty';
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] !== 'number') {
+            countNotNum++;
         } else {
-            sum += item;
+            sum += arr[i];
         }
     }
-    return sum / arr.length;
+    if (countNotNum == 0) {
+        return sum / arr.length;
+    } else {
+        return sum / (arr.length - countNotNum);
+    }
 }
 console.log(caclculateAverage(10, 20, 30));
 console.log(caclculateAverage());
 console.log(caclculateAverage(4, 20, 15));
+console.log(caclculateAverage(10, 'a', 30));
